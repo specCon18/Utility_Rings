c2d3d991b187a1e953c9737eff204ad5d77660ba
Steven <specCon18@gmail.com>
2023-10-01 22:39:26 -0400

    VolcanicGlassRing addded right click logic, Added Curios

--- src/main/java/com/skdevstudios/util_rings/items/VolcanicGlassRing.java
+++ src/main/java/com/skdevstudios/util_rings/items/VolcanicGlassRing.java
@@ -1,9 +1,15 @@
 package com.skdevstudios.util_rings.items;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
@@ -13,9 +19,24 @@ public class VolcanicGlassRing extends Item implements ICurioItem {
     }
     
     @Override
-    public InteractionResult useOn(UseOnContext context){
-        return null;
-        //TODO: Add right click spawns obsidian block
+    public InteractionResult useOn(UseOnContext context) {
+        Level world = context.getLevel();
+        BlockPos pos = context.getClickedPos().relative(context.getClickedFace());
+
+        BlockState targetState = world.getBlockState(pos);
+
+        if (targetState.isAir() && !world.isClientSide()) {
+            // Check if the target block is air and place obsidian
+            BlockState obsidian = Blocks.OBSIDIAN.defaultBlockState();
+            world.setBlockAndUpdate(pos, obsidian);
+            
+            // Play a sound effect at the placed block position
+            world.playSound(null, pos, SoundEvents.STONE_PLACE, SoundSource.BLOCKS, 1.0F, 1.0F);
+
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.FAIL;
     }
     @Override
     public void curioTick(SlotContext slotContext, ItemStack stack) {
