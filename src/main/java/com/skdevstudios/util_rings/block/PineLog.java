9a1395d7b0a97fb276ba8d50d8b9db33d05e6e82
Steven <specCon18@gmail.com>
2023-10-03 03:38:20 -0400

    added horizontal facing need to fix vertical for pinelog

--- src/main/java/com/skdevstudios/util_rings/block/PineLog.java
+++ src/main/java/com/skdevstudios/util_rings/block/PineLog.java
@@ -1,21 +1,30 @@
 package com.skdevstudios.util_rings.block;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 public class PineLog extends Block {
     public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
 
     public PineLog(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.defaultBlockState().setValue(AXIS, Direction.Axis.Y).setValue(FACING, Direction.NORTH));
     }
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block,BlockState> builder){
         super.createBlockStateDefinition(builder);
-        builder.add(AXIS);
+        builder.add(AXIS,FACING);
     }
     
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getHorizontalDirection();
+        return this.defaultBlockState().setValue(AXIS, direction.getAxis()).setValue(FACING, direction);
+    }
 }
