79f304521b786b726aa46ad2a271e61274c9f94b
Steven <specCon18@gmail.com>
2023-10-02 21:04:02 -0400

    added loot tables and correct tools to break blocks

--- src/main/java/com/skdevstudios/util_rings/init/BlockInit.java
+++ src/main/java/com/skdevstudios/util_rings/init/BlockInit.java
@@ -4,7 +4,9 @@ import com.skdevstudios.util_rings.UtilRings;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,5 +16,13 @@ public class BlockInit {
 
     public static final RegistryObject<Block> LEAD_ORE = BLOCKS.register("lead_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
     public static final RegistryObject<Block> TIN_ORE = BLOCKS.register("tin_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
-    public static final RegistryObject<Block> PINE_LOG = BLOCKS.register("pine_log", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PINE_LOG = BLOCKS.register("pine_log",
+        () -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.COLOR_BROWN)
+            .sound(SoundType.WOOD)
+            .strength(2)
+            .explosionResistance(2)
+            .requiresCorrectToolForDrops()
+        )
+    );
 }
